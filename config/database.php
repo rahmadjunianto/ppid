f2f8c594b56b5e708be596f962acc7566e60ad7a
ppidkemenagnganjuk <ppid.kemenagnganjuk@gmail.com>
2025-08-29 11:17:11 +0000

    db

--- config/database.php
+++ config/database.php
@@ -63,6 +63,26 @@ return [
             ]) : [],
         ],
 
+'mysql_portal' => [
+            'driver' => 'mysql',
+            'url' => env('DATABASE_URL'),
+            'host' => env('DB_PORTALHOST', '127.0.0.1'),
+            'port' => env('DB_PORTALPORT', '3306'),
+            'database' => env('DB_PORTALDATABASE', 'forge'),
+            'username' => env('DB_PORTALUSERNAME', 'forge'),
+            'password' => env('DB_PORTALPASSWORD', ''),
+            'unix_socket' => env('DB_PORTALSOCKET', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'strict' => true,
+            'engine' => null,
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+            ]) : [],
+        ],
+
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
